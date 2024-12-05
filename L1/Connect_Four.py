c92340fc4a8a8569f794207bf9aa606675bdcfd5
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-12-06 00:43:00 +1300

    processing empty list

--- L1/Connect_Four.py
+++ L1/Connect_Four.py
@@ -2,10 +2,11 @@ from pprint import pprint
 n_rows_ = 6
 n_columns_ = 7
 
-def init_field(rows=n_rows_, columns=n_columns_): # test commit
-    for i in rows:
-        for j in columns:
-            pass     
+def init_field(rows=n_rows_, columns=n_columns_):
+
+    field = [['.' for y in range(columns)] for x in range(rows)]
+    return field
+
 
 
 def drop_token(field, col, player, symbol={True: 'x', False: 'o'}):
@@ -20,7 +21,8 @@ def play(field, tokens):
 
 
 if __name__ == "__main__":
-    field, tokens = init_field()
+    # field, tokens = init_field()
+    field= init_field()
     pprint(field)
     winner = play(field, tokens)
     print(f"Player {winner} wins the game. Congratulations!")
