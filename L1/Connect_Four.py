27e968a4336bf00ff6dd5227f2e66308499f7085
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2024-12-09 22:33:21 +1300

    processed symbol matches

--- L1/Connect_Four.py
+++ L1/Connect_Four.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 n_rows_ = 6
 n_columns_ = 7
+username1 = True
+active_player = username1
+username2 = False
 
 def init_field(rows=n_rows_, columns=n_columns_):
     
@@ -12,46 +15,74 @@ def drop_token(field, col, player, symbol={True: 'x', False: 'o'}):
 
     global n_rows_
 
-    for i in field[range(n_rows_)][col]:
-        if field[i+1][col] != '.':
+    for i in range(n_rows_-1,-1,-1):
+        if field[i][col] == '.' and i == n_rows_-1:
             field[i][col] = symbol.get(player)
-            
-        
+            break
+        elif field[i][col] != '.' and field[i-1][col] == '.':
+            field[i-1][col] = symbol.get(player)
+            break
+     
 def game_is_won(field, void='.'):
 
     # returns bool
     global n_rows_, n_columns_
 
-    r = n_rows_
-    c = n_columns_
+    for r in range(n_rows_): #vertical
+        for c in range(n_columns_):
+            if (r-3 >= n_rows_-n_rows_):
+                if field[r][c] != void and field[r][c] == field[r-1][c] == field[r-2][c] == field[r-3][c] :
+                    return True
 
-    for i in range(r): #vertical
-        for j in range(c):
-            if field[r][c] != void and field[r][c] == field[r+1][c] == field[r+2][c] == field[r+3][c]:
-                return True
+    for r in range(n_rows_): #horiz
+        for c in range(n_columns_):
+            if c+3 < n_columns_:
+                if field[r][c] != void and field[r][c] == field[r][c+1] == field[r][c+2] == field[r][c+3]:
+                    return True
 
-    for i in range(n_rows_): #horiz
-        for j in range(n_columns_):
-            if field[r][c] != void and field[r][c] == field[r][c+1] == field[r][c+2] == field[r][c+3]:
-                return True
+    for r in range(n_rows_): #NE
+        for c in range(n_columns_):
+            if (r-3 >= n_rows_-n_rows_) and (c+3 < n_columns_):
+                if (field[r][c] != void and field[r][c] == field[r-1][c+1] == field[r-2][c+2] == field[r-3][c+3] 
+                and r-3 < n_rows_ and c+3 < n_columns_):
+                    return True
 
-    for i in range(n_rows_): #NE
-        for j in range(n_columns_):
-            if field[r][c] != void and field[r][c] == field[r-1][c+1] == field[r-2][c+2] == field[r-3][c+3]:
-                return True
-
-    for i in range(n_rows_): #NW
-        for j in range(n_columns_):
-            if field[r][c] != void and field[r][c] == field[r-1][c-1] == field[r-1][c-1] == field[r-1][c-1]:
-                return True
+    for r in range(n_rows_): #NW
+        for c in range(n_columns_):
+            if (r-3 >= n_rows_-n_rows_) and (c-3 >= n_columns_-n_columns_):
+                if (field[r][c] != void and field[r][c] == field[r-1][c-1] == field[r-1][c-1] == field[r-1][c-1]
+                    and c-3 < n_columns_):
+                    return True
 
     return False
 
+def switch_player():
+    global active_player
+    if active_player == username1:
+        active_player = username2
+    else:
+        active_player = username1
+
 def play(field, tokens):
 
-    while(not game_is_won(field, void='.')):
-          pass
+    global n_columns_
+    username1 = input("Please enter your name, player 1: ")
+    username2 = input("Please enter your name, player 2: ")
+    player = {username1:True, username2:False}
 
+    active_player = username1
+    while(not game_is_won(field, void='.')):
+
+        col = int(input(f"Player {active_player}, please enter your column between 0 to 6: "))
+        while((col < 0) or (col >= n_columns_ )):
+            col = int(input(f"Player {active_player}, please try again entering between 0 to 6: "))
+        drop_token(field,col,player.get(active_player),tokens)
+        pprint(field)
+        switch_player()
+
+    switch_player()
+    winner = active_player
+    return winner 
 
 if __name__ == "__main__":
     field, tokens = init_field()
