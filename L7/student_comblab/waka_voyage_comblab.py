d09a0a18006c5e20d551669c463bed2b4586d0e5
Rounak Giri <186711485+darkhorse1081@users.noreply.github.com>
2025-07-24 23:41:10 +1200

    Update waka_voyage_comblab.py

--- L7/student_comblab/waka_voyage_comblab.py
+++ L7/student_comblab/waka_voyage_comblab.py
@@ -9,8 +9,20 @@ network.read_network('waka_voyage_network.txt')
 
 # TODO - your code here in Task 3
 travel_time, path = shortest_path(network, 'Taiwan', 'Hokianga')
-
-# iterate across each pair of nodes
-# does the source have to be == Taiwan?
+network_nodes = []
+list_times = []
 
 # check path pairs with the source being Taiwan always and destination varying -> sort out that list
+# one source -> paired with all other destinations -> regardless in path or not ()
+for i in network.nodes:
+     for j in network_nodes:
+          if i != j:
+               network_nodes.append((i,j))
+               list_times.append(shortest_path(network,i.name,j.name))
+          else:
+               continue
+          
+sorted_corresponding = [x for y, x in sorted(zip(node_distance, revision_list))]
+node_recieved = network.get_node(sorted_corresponding[0])
+
+# finding the greatest travle distance
