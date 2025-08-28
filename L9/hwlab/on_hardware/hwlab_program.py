465b34f96fd535820141b49c022dd102bbf2053d
Rounak Giri <186711485+darkhorse1081@users.noreply.github.com>
2025-08-28 17:35:44 +1200

    Update hwlab_program.py

--- L9/hwlab/on_hardware/hwlab_program.py
+++ L9/hwlab/on_hardware/hwlab_program.py
@@ -1,16 +1,17 @@
 '''
-Hardware Lab device driver exercise 4.
-
 Moves a pixel around the display on a micro:bit.
 '''
-
 import microbit
 import hwlab_driver
+
 
 while True:
     for n in range(25):
         x = int(n/5)
         y = n % 5
-        #TODO: call function in device driver to light pixel in row x, column y
+        hwlab_driver.display_pixel(x,y)
         microbit.sleep(500)
+        hwlab_driver.clear_display()
+
+
     
